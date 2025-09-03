477f7fb08884a28fb4394912484963c717e64635
Sambhav <sambhav12wakhariya@gmail.com>
2025-09-03 18:53:31 +0530

    Tested Routes and Types of http requests.

--- src/server.js
+++ src/server.js
@@ -10,6 +10,22 @@ app.get('/test', (req, res)=>{
     res.send("This is Test Route for Application");
 })
 
+app.get('/profile', (req, res)=> {
+    res.send({name :  "Sambhav", age: 21});
+})
+
+app.post('/profile', (req, res)=>{
+    res.send({message: "This is a post request"});
+})
+
+app.patch('/profile', (req, res)=>{
+    res.send({message: "This is a patch request"});
+});
+
+app.delete('/profile', (req, res)=> {
+    res.send({message: "This is a delete request"});
+})
+
 app.listen(3000, ()=> {
     console.log('Server is running on port 3000');
 });
