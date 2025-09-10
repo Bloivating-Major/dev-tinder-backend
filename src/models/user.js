4782d32f8f61128a7ed04a2c4fca7a6b787d5315
Sambhav <sambhav12wakhariya@gmail.com>
2025-09-10 23:30:00 +0530

    Validator Library

--- src/models/user.js
+++ src/models/user.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const validate = require('validator');
 
 const userSchema = new mongoose.Schema({
     firstName : {
@@ -17,13 +18,23 @@ const userSchema = new mongoose.Schema({
         required : [true, 'Email is required'],
         unique : true,
         lowercase : true,
-        trim : true
+        trim : true,
+        validate(value){
+            if(!validate.isEmail(value)){
+                throw new Error('Invalid Email Address: ' + value);
+            }
+        }
     },
     password : {
         type : String,
         required : true,
         minLength : 6,
-        maxLength : 1024
+        maxLength : 1024,
+        validate(value){
+            if(!validate.isStrongPassword(value)){
+                throw new Error('Please Enter a Strong Password: '+ value);
+            }
+        }
     },
     age : {
         type : Number,
@@ -41,6 +52,11 @@ const userSchema = new mongoose.Schema({
     photoURL : {
         type : String,
         default : "https://i.pinimg.com/736x/91/b3/8a/91b38a3ac3937419fc9bafcb7d7d1c4a.jpg",
+        validate(value){
+            if(!validate.isURL(value)){
+                throw new Error('Invalid Photo URL: ' + value);
+            }
+        }
     },
     about : {
         type : String,
